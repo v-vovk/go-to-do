5373dd0ac05a57422f52f56fefb9e013b6ecb6fe
Vlad Vovk <wolfykey@gmail.com>
2021-06-08 22:10:47 +0300

    dependency injections

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	todo "github.com/wolfykey/go-to-do"
 	"github.com/wolfykey/go-to-do/pkg/handler"
+	"github.com/wolfykey/go-to-do/pkg/repository"
+	"github.com/wolfykey/go-to-do/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewSRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server on port: %s", err.Error())
