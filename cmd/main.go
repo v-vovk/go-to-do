51586cf4c563eb8fe53247612a40e6233a150fad
Vlad Vovk <wolfykey@gmail.com>
2021-06-08 22:22:04 +0300

    add config.yml

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	todo "github.com/wolfykey/go-to-do"
 	"github.com/wolfykey/go-to-do/pkg/handler"
 	"github.com/wolfykey/go-to-do/pkg/repository"
@@ -9,12 +10,22 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewSRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server on port: %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
