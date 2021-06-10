0ee3e7ad4f5a64ea6764d31856b45653bb7fbda4
Vlad Vovk <wolfykey@gmail.com>
2021-06-10 15:24:36 +0300

    add connection to db postgres

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	todo "github.com/wolfykey/go-to-do"
 	"github.com/wolfykey/go-to-do/pkg/handler"
 	"github.com/wolfykey/go-to-do/pkg/repository"
 	"github.com/wolfykey/go-to-do/pkg/service"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,7 +17,23 @@ func main() {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewSRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize to db: %s", err.Error())
+	}
+
+	repos := repository.NewSRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Authorization interface {
 }
 
@@ -15,6 +17,6 @@ type Repository struct {
 	TodoItem
 }
 
-func NewSRepository() *Repository {
+func NewSRepository(db *sqlx.DB) *Repository {
 	return &Repository{}
 }
